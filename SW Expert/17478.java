27ae09ec11edd1d3329d6e3b51462fa190457f55
hyuntohoon <81351526+hyuntohoon@users.noreply.github.com>
2024-01-29 12:25:19 +0900

    fix : SW Expert 1289.java

--- SW Expert/17478.java
+++ SW Expert/17478.java
@@ -2,13 +2,11 @@ package com.ssafy.daily01;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main_실버5_17478_재귀함수가뭔가요_박지훈 {
-	public Main_실버5_17478_재귀함수가뭔가요_박지훈() throws NumberFormatException, IOException {
-		System.setIn(new FileInputStream("1204_input.txt"));
+public class Solution_D3_1289_원재의메모리복구하기_박지훈 {
+	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(in.readLine());
 		for(int t=1;t<T+1;t++) {
