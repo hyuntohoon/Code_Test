a49815c2a8b096da940ab2a2f8cfdd6c1bfdf105
hyuntohoon <81351526+hyuntohoon@users.noreply.github.com>
2024-01-30 15:24:38 +0900

    feat : SW expert 1208.java

---
+++ SW Expert/1208.java
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <string>
+using namespace std;
+typedef unsigned long long ull;
+typedef long long ll;
+
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { -1,0,1,0 };
+
+int N, M;
+int startX, startY, startD;
+int roomArr[51][51];
+void input() {
+	ios::sync_with_stdio;
+	cin.tie(NULL);
+	cin >> N >> M;
+	for (int i = 0; i < N;i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> roomArr[i][j];
+		}
+	}
+}
+void go() {
+
+	while (true) {
+		if() // 청소한다.
+
+		if() // 칸이 없는 경우 
+			if // 후진 가능
+			if // 후진 불가능
+
+		if // 
+	}
+}
+int main() {
+
+
+}
