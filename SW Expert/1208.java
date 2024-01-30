b555dd5e7ef9d0094320e680b21cf2c8bf9bfa0c
hyuntohoon <81351526+hyuntohoon@users.noreply.github.com>
2024-01-30 15:25:09 +0900

    feat : SW expert 1208.java

--- SW Expert/1208.java
+++ SW Expert/1208.java
@@ -1,43 +1,45 @@
-#include <iostream>
-#include <math.h>
-#include <stdio.h>
-#include <algorithm>
-#include <queue>
-#include <vector>
-#include <string>
-using namespace std;
-typedef unsigned long long ull;
-typedef long long ll;
+package com.ssafy.daily02;
 
-int dx[4] = { 0,1,0,-1 };
-int dy[4] = { -1,0,1,0 };
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
-int N, M;
-int startX, startY, startD;
-int roomArr[51][51];
-void input() {
-	ios::sync_with_stdio;
-	cin.tie(NULL);
-	cin >> N >> M;
-	for (int i = 0; i < N;i++) {
-		for (int j = 0; j < M; j++) {
-			cin >> roomArr[i][j];
+public class Main_Solution_D4_1210_Ladder1 {
+	class reverseComparator implements Comparator<Integer> {
+	    @Override
+	    public int compare(Integer a, Integer b) {
+	        if(a > b) return a;
+	        else return b;
+	    }
+	}
+	public static void main(String[] args) throws IOException, NumberFormatException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int T = 1;
+		while(true) {
+			if(T ==11) break;
+			List<Integer> list = new ArrayList<>();
+			int dump = Integer.parseInt(in.readLine());
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			for(int i = 0;i<100;i++) {
+				Integer a = new Integer(st.nextToken());
+				list.add(Integer.valueOf(a));
+			}
+			Collections.sort(list);
+			while(true) {
+				if(dump == 0) break;
+				list.set(99, list.get(99)-1);
+				list.set(0, list.get(0)+1);
+				Collections.sort(list);
+				dump--;
+			}
+			System.out.println("#" + T + " " + (list.get(99)-list.get(0)));
+			T++;
 		}
 	}
-}
-void go() {
-
-	while (true) {
-		if() // 청소한다.
-
-		if() // 칸이 없는 경우 
-			if // 후진 가능
-			if // 후진 불가능
-
-		if // 
-	}
-}
-int main() {
-
-
 }
