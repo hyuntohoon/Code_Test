8020e5ebb319954589589df7006d6733ccfcbf68
hyuntohoon <81351526+hyuntohoon@users.noreply.github.com>
2024-07-05 22:55:43 +0900

    feat: baekjoon 1094.java

---
+++ BACKJOON/1094.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        System.out.println(Integer.bitCount(X));
+    }
+}
