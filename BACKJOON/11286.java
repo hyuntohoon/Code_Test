413fdf95e39d6c63724c1ea89ddf698fa1b1566d
hyuntohoon <81351526+hyuntohoon@users.noreply.github.com>
2024-07-22 22:53:17 +0900

    Update 11286.java

--- BACKJOON/11286.java
+++ BACKJOON/11286.java
@@ -3,7 +3,7 @@ package daily08;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.PriorityQueue; 
 
 import javax.rmi.CORBA.StubDelegate;
 
