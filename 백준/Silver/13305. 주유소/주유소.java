6597fcae869651ba9bf49ff1ad619321f8a6c2f2
hyuntohoon <81351526+hyuntohoon@users.noreply.github.com>
2025-09-10 17:35:02 +0900

    [Silver III] Title: 주유소, Time: 364 ms, Memory: 36296 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,40 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       int N = Integer.parseInt(br.readLine());
-       int []V = new int[N-1];
-       int []E = new int[N];
-       StringTokenizer st = new StringTokenizer(br.readLine());
-       for(int i = 0; i < N-1; i++) {
-           V[i] = Integer.parseInt(st.nextToken());
-       }
-       st = new StringTokenizer(br.readLine());
-       for(int i = 0; i < N; i++) {
-           E[i] = Integer.parseInt(st.nextToken());
-       }
-       int []DP = new int[N];
-        Arrays.fill(DP, Integer.MAX_VALUE);
-       for(int i = 0; i < N; i++) {
-           for(int j = i; j < N-1; j++) {
-               DP[j] = Math.min(DP[j], V[j]*E[i]);
-               //System.out.print(DP[j] + " " + i+ " " + j + " ");
-           }
-          // System.out.println();
-       }
-       int sum = 0;
-       for (int i =0; i < N-1; i++) {
-           //System.out.print(DP[i] + " ");
-           sum += DP[i];
-       }
-       System.out.println(sum);
+        int N = Integer.parseInt(br.readLine());
+
+        long[] dist = new long[N - 1];
+        long[] price = new long[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N - 1; i++) dist[i] = Long.parseLong(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) price[i] = Long.parseLong(st.nextToken());
+
+        long minPrice = price[0];
+        long total = 0;
+
+        for (int i = 0; i < N - 1; i++) {
+            if (price[i] < minPrice) minPrice = price[i]; // 현재 도시 가격 반영
+            total += minPrice * dist[i];
+        }
+
+        System.out.println(total);
     }
 }
