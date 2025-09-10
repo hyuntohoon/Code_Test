714520c79353425d29d7ba88da1e1cb1277533ad
hyuntohoon <81351526+hyuntohoon@users.noreply.github.com>
2025-09-10 16:47:24 +0900

    [Silver V] Title: 숫자 카드, Time: 3108 ms, Memory: 161868 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static ArrayList<Integer> al;
+    public static int bin(int n) {
+        int start = 0;
+        int end = al.size();
+        while(start < end) {
+            int mid = start + (end- start) / 2;
+            if(al.get(mid) == n) return 1;
+            else if(al.get(mid)> n) end = mid;
+            else start = mid + 1;
+        }
+        return 0;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        al = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+       for (int i = 0; i < N; i++) {
+
+            al.add(Integer.parseInt(st.nextToken()));
+       }
+        al.sort((a, b) -> a - b);
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < M; i++) {
+
+            int a= Integer.parseInt(st.nextToken());
+            System.out.print(bin(a)+ " ");
+        }
+    }
+}
