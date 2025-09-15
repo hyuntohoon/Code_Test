7fc28c32a2882d0d2da81b7cf8ab335d794d05f7
hyuntohoon <81351526+hyuntohoon@users.noreply.github.com>
2025-09-15 15:26:14 +0900

    [Gold V] Title: 집합의 표현, Time: 884 ms, Memory: 57340 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,53 +1,54 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
-    public static int[] parent;
-    public static int[] rank;
-    public static int N,M;
-   public static void main(String[] args) throws IOException {
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      StringTokenizer st = new StringTokenizer(br.readLine());
-      N = Integer.parseInt(st.nextToken());
-      M = Integer.parseInt(st.nextToken());
-      parent = new int[N+1];
-      for (int i = 1; i <= N; i++) {
-          parent[i] = i;
-      }
-      rank = new int[N+1];
-      for(int i=0;i<M;i++){
-          st = new StringTokenizer(br.readLine());
-          int temp = Integer.parseInt(st.nextToken());
-          int a = Integer.parseInt(st.nextToken());
-          int b = Integer.parseInt(st.nextToken());
-          if(temp == 0){
-            union(a,b);
-          }
-          if(temp == 1){
-            if(findP(a) == findP(b)) System.out.println("YES");
-            else System.out.println("NO");
-          }
-      }
-   }
-   public static int findP(int x){
-       if(parent[x] != x){ // 부모가 아니면
-           parent[x] = findP(parent[x]);
-       }
-       return parent[x];
-   }
-   public static void union(int x, int y){
-      int rootX = findP(x);
-      int rootY = findP(y);
-      if(rootX != rootY){
-          if(rank[rootX] > rank[rootY]){
-              parent[rootY] = rootX;
-          }
-          else if(rank[rootX]>rank[rootY]) parent[rootX] = rootY;
-          else{
-              parent[rootX] = rootY;
-              rank[rootX]++;
-          }
-      }
-   }
+    public static int []parents;
+    public static int []rank;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        parents = new int[N+1];
+        rank = new int[N+1];
+        int M = Integer.parseInt(st.nextToken());
+        for(int i = 1; i <= N; i++) {
+            parents[i] = i;
+        }
+        for(int i =0;i<M;i++){
+            st = new StringTokenizer(br.readLine());
+            int tiger = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if(tiger==1){
+                if(findP(a) == findP(b)) System.out.println("YES");
+                else System.out.println("NO");
+            }
+            else if(tiger==0){
+                unionA(a,b);
+            }
+        }
+    }
+
+    private static void unionA(int a, int b) {
+        int rootA = findP(a);
+        int rootB = findP(b);
+        if(rootA==rootB) return;
+        if(rank[rootA] > rank[rootB]){
+            parents[rootB]=rootA;
+        }
+        else if (rank[rootA] < rank[rootB]){
+            parents[rootA]=rootB;
+        }
+        else{
+            parents[rootA]=rootB;
+            rank[rootB]++;
+        }
+    }
+
+    private static int findP(int a) {
+        if(parents[a] != a){
+            parents[a] = findP(parents[a]);
+        }
+        return parents[a];
+    }
 }
